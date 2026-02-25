74ca9b9434f80e396c165ca1884078ed4f98bd77
Avalon <210815572+Avalon96@users.noreply.github.com>
2026-02-25 12:10:14 +0300

    optimized brute force

--- brute_force.c
+++ brute_force.c
@@ -3,18 +3,12 @@
 
 int		brute_force(t_ps *ps)
 {
-	float	disorder;
 	int		flag;
 
 	if (ps->a.size > 5)
 		return (0);
 	flag = ps->a.size - 3;
-	if (ps->a.size == 5)
-	{
-		rotate_a_to_top(ps, get_max_pos(&ps->a));
-		push_b(ps);
-	}
-	if (ps->a.size == 4)
+	while (ps->a.size > 3)
 	{
 		rotate_a_to_top(ps, get_min_pos(&ps->a));
 		push_b(ps);
@@ -22,8 +16,5 @@ int		brute_force(t_ps *ps)
 	sort_three_a(ps);
 	while (flag--)
 		push_a(ps);
-	compute_disorder(&ps->a, &disorder);
-	if (disorder != 0)
-		rotate_a_to_top(ps, get_min_pos(&ps->a));
 	return (1);
 }
